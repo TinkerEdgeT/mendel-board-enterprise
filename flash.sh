3ca48b558f9b501c8b4a558729f96b55795ccd5a
Dmitry Kovalev <dkovalev@google.com>
2018-08-24 14:37:35 -0700

    Flash partition table image according to eMMC size.
    Change-Id: I0c00905355ffdad91d068f5264aff5178d9921eb

--- flash.sh
+++ flash.sh
@@ -13,11 +13,30 @@ else
     PRODUCT_OUT=${PRODUCT_OUT:=${ROOTDIR}/out/target/product/imx8m_phanbell}
 fi
 
+function partition_table_image {
+    case $1 in
+    7818182656)
+        echo "partition-table-8gb.img" ;;
+    15634268160)
+        echo "partition-table-16gb.img" ;;
+    62537072640)
+        echo "partition-table-64gb.img" ;;
+    *)
+        echo "invalid-partition-table-$1.img" ;;
+    esac
+}
 
+# Flash bootloader
 fastboot flash bootloader0 ${PRODUCT_OUT}/u-boot.imx
-fastboot flash gpt ${PRODUCT_OUT}/partition-table.img
 fastboot reboot-bootloader
-fastboot erase misc
-fastboot flash boot ${PRODUCT_OUT}/boot.img
-fastboot flash rootfs ${PRODUCT_OUT}/rootfs.img
-fastboot reboot
+
+# Flash partition table
+MMC_SIZE=$(fastboot getvar mmc_size 2>&1 | grep mmc_size | cut -d " " -f 2)
+fastboot flash gpt ${PRODUCT_OUT}/$(partition_table_image ${MMC_SIZE})
+fastboot reboot-bootloader
+
+# Flash filesystems
+#fastboot erase misc
+#fastboot flash boot ${PRODUCT_OUT}/boot.img
+#fastboot flash rootfs ${PRODUCT_OUT}/rootfs.img
+#fastboot reboot
