cf0071b7ed83182bb387aed84afbcf1aa3345d4c
June Tate-Gans <jtgans@google.com>
2018-12-10 12:31:36 -0800

    jump-targets: Add board-specific jump targets.
    Change-Id: I6e8fa9467b7e016ea9d2f76eb7496333f643c634

---
+++ jump_targets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+JUMP_TARGETS[kernel]="${ROOTDIR}/linux-imx/"
+JUMP_TARGETS[uboot]="${ROOTDIR}/uboot-imx/"
+JUMP_TARGETS[kernel-package]="${ROOTDIR}/packages/linux-imx"
+JUMP_TARGETS[uboot-package]="${ROOTDIR}/packages/uboot-imx"
+
